846e77e1d85c1e7fb14525346300d8f0b6465cb5
Ibrewcoffee Onthemoon <ibrewcoffeeonthemoon@gmail.com>
2026-02-01 07:06:17 +0800

    renames, comments

--- ac-drift-monitor.py
+++ ac-drift-monitor.py
@@ -15,12 +15,18 @@ from app import App
 # This function gets called by AC when the Plugin is initialised
 # The function has to return a string with the plugin name
 def acMain(ac_version: str) -> str:
+    # create App
     global app
     app = App()
-    ac.addRenderCallback(app.win, onFormRender)
+
+    # register render callback
+    ac.addRenderCallback(app.win, onRender)
+
+    # return App name
     return app.name
 
 
-def onFormRender(deltaT: float) -> None:
+def onRender(deltaT: float) -> None:
+    # render
     global app
     app.render()
