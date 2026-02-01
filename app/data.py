7e8da4e1a549183e14ddc13730835ed1c0b664f2
Ibrewcoffee Onthemoon <ibrewcoffeeonthemoon@gmail.com>
2026-02-01 19:44:59 +0800

    data module, Telemetry class

---
+++ app/data.py
@@ -0,0 +1,25 @@
+import ac
+import acsys
+
+
+class _AccG:
+    def __init__(self) -> None:
+        self.x = 0.0
+        self.y = 0.0
+        self.z = 0.0
+
+    def fetch(self, car_id: int) -> None:
+        self.x, self.y, self.z = ac.getCarState(car_id, acsys.CS.AccG)
+
+
+class Telemetry:
+
+    def __init__(self) -> None:
+        # data
+        self.accG = _AccG()
+
+        # init
+        self._car_id = ac.getFocusedCar()
+
+    def fetch(self) -> None:
+        self.accG.fetch(self._car_id)
