f168e10e0443dbcef7a0b9497eb50b06320ec30d
Ibrewcoffee Onthemoon <ibrewcoffeeonthemoon@gmail.com>
2026-02-02 02:32:48 +0800

    success display line

--- app/components/accG_bar.py
+++ app/components/accG_bar.py
@@ -1,10 +1,14 @@
 from app.components.lib.indicator import Indicator
+from app.components.lib.rectangle import Rectangle
 from app.data import telemetry
 
 
 class AccG_Bar:
 
     def __init__(self) -> None:
+        # shapes
+        self._line = Rectangle(0, 99, 333, 5)
+
         # create indicators
         self._latG = Indicator(
             x_pos=22,
@@ -21,5 +25,5 @@ class AccG_Bar:
         )
 
     def render(self) -> None:
-        # set indicator values
+        self._line.render()
         self._latG.value, _, self._longG.value = telemetry.accG
