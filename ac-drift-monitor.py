05d191af13038b1dc4b8753dac5fe9bfc9059537
Ibrewcoffee Onthemoon <ibrewcoffeeonthemoon@gmail.com>
2026-02-02 01:24:03 +0800

    main and app call from window

--- ac-drift-monitor.py
+++ ac-drift-monitor.py
@@ -2,16 +2,17 @@ import ac
 
 from app import app
 from app.data import telemetry
+from app.window import name, window
 
 
 # This function gets called by AC when the Plugin is initialised
 # The function has to return a string with the plugin name
 def acMain(ac_version: str) -> str:
     # register render callback
-    ac.addRenderCallback(app.window, on_render)
+    ac.addRenderCallback(window, on_render)
 
     # return App name
-    return app.name
+    return name
 
 
 def acUpdate(deltaT: float) -> None:
