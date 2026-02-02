cbc6965ae45ee7958e50c0a14f4ddadc7bbce848
Ibrewcoffee Onthemoon <ibrewcoffeeonthemoon@gmail.com>
2026-02-02 22:52:11 +0800

    clipping and smoothing

--- app/components/accG_grid.py
+++ app/components/accG_grid.py
@@ -11,16 +11,31 @@ class AccG_Grid:
         width: int,
         height: int,
         dot_size: int,
+        max_value: float = 2.0,
     ) -> None:
         self._x_pos = x_pos
         self._y_pos = y_pos
         self._width = width
         self._height = height
         self._dot_size = dot_size
+        self._max_value = max_value
+        self._x_old = 0.0
+        self._z_old = 0.0
 
     def render(self) -> None:
         # fetch telemetry
-        x, _, z = telemetry.accG
+        x_raw, _, z_raw = telemetry.accG
+
+        # clipping
+        x_clipped = min(max(x_raw, -self._max_value), self._max_value)
+        z_clipped = min(max(z_raw, -self._max_value), self._max_value)
+
+        # smoothing
+        weight = 0.8
+        x = weight * x_clipped + (1-weight) * self._x_old
+        z = weight * z_clipped + (1-weight) * self._z_old
+        self._x_old = x_clipped
+        self._z_old = z_clipped
 
         # draw a centered box on a 2D grid, visualize the g-force
         def box_pos(val: float, start: int, width: int, box_width: int) -> int:
