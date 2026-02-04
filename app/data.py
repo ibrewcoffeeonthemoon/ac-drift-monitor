59a30a31efa45269503924353ecc886fa085a5f9
Ibrewcoffee Onthemoon <ibrewcoffeeonthemoon@gmail.com>
2026-02-04 18:57:54 +0800

    _Data itf

--- app/data.py
+++ app/data.py
@@ -1,16 +1,31 @@
+from abc import ABCMeta, abstractmethod
+
 import ac
 import acsys
 
 
-class _AccG:
+class _Data(metaclass=ABCMeta):
     def __init__(self) -> None:
+        self._data = ()  # type: tuple[float, ...]
+
+    @abstractmethod
+    def fetch(self, car_id: int) -> None:
+        ...
+
+    def __iter__(self):
+        yield from self._data
+
+    def __getitem__(self, index: int) -> float:
+        return self._data[index]
+
+
+class _AccG(_Data):
+    def __init__(self) -> None:
+        super().__init__()
         self._data = (0.0, 0.0, 0.0)
 
     def fetch(self, car_id: int) -> None:
         self._data = ac.getCarState(car_id, acsys.CS.AccG)
-
-    def __iter__(self):
-        yield from self._data
 
     @property
     def x(self) -> float: return self._data[0]
@@ -22,18 +37,13 @@ class _AccG:
     def z(self) -> float: return self._data[2]
 
 
-class _SlipRatio:
+class _SlipRatio(_Data):
     def __init__(self) -> None:
+        super().__init__()
         self._data = (0.0, 0.0, 0.0, 0.0)
 
     def fetch(self, car_id: int) -> None:
         self._data = ac.getCarState(car_id, acsys.CS.SlipRatio)
-
-    def __iter__(self):
-        yield from self._data
-
-    def __getitem__(self, index: int) -> float:
-        return self._data[index]
 
     @property
     def fl(self) -> float: return self._data[0]
