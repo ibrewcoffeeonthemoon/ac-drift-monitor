2d470951d8a4f773c6d01ce9316c5474513a3f6d
Ibrewcoffee Onthemoon <ibrewcoffeeonthemoon@gmail.com>
2026-02-06 04:32:57 +0800

    set window size after all components are defined

--- app/__init__.py
+++ app/__init__.py
@@ -12,12 +12,6 @@ class _App:
         width: int,
         height: int,
     ) -> None:
-        # set layouts, styles
-        ac.setSize(window, width, height)
-        ac.setTitle(window, '')
-        ac.setIconPosition(window, 0, -10000)
-        ac.drawBorder(window, False)
-
         # create components
         self._slip_ratio_monitor = SlipRatioMonitor(
             x_pos=0,
@@ -31,6 +25,13 @@ class _App:
             width=width//2,
             height=height,
         )
+
+        # TODO: dynamically resolve width and height from the component above, no hard code
+        # set layouts, styles
+        ac.setSize(window, width, height)
+        ac.setTitle(window, '')
+        ac.setIconPosition(window, 0, -10000)
+        ac.drawBorder(window, False)
 
     def render(self) -> None:
         self._gforce_monitor.render()
