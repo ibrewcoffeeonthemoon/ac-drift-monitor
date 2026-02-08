9be4fb76dafc8243ef933b4cf0a794da0f30eea6
Ibrewcoffee Onthemoon <ibrewcoffeeonthemoon@gmail.com>
2026-02-09 07:17:22 +0800

    fixed slots, no __dict__ for this object, saves memory footprint

--- app/lib/geometry.py
+++ app/lib/geometry.py
@@ -4,6 +4,9 @@ NOTE: on python 3.3.5, tuple subclass is roughly 2.5x to 3x slower than native t
 
 
 class Tuple2i(tuple):
+    # fixed slots, no __dict__ for this object, saves memory footprint
+    __slots__ = ()
+
     def __new__(cls, x: int, y: int,) -> 'Tuple2i':
         return super().__new__(cls, (x, y))
 
