70e3fd9f3d9ef5e0a3e7ce1334a00dac412b3970
Ibrewcoffee Onthemoon <ibrewcoffeeonthemoon@gmail.com>
2026-02-09 23:59:16 +0800

    use absolute dll_path to avoid PATH hack, simplify import logics

--- app/telemetry/memory/__init__.py
+++ app/telemetry/memory/__init__.py
@@ -2,40 +2,12 @@ import os
 import platform
 import sys
 
-import ac
-
-
-def _init1():
-    app_dir = os.path.dirname(__file__)
-    sysdir = os.path.join(app_dir, 'dll', 'stdlib64')
-    sys.path.insert(0, sysdir)
-    os.environ['PATH'] = os.environ['PATH'] + ";."
-    ac.log(sysdir)
-    # print: apps/python/ac-drift-monitor\app\telemetry\memory\dll\stdlib64
-    from .lib.sim_info import info
-    return info
-
-
-def _init2():
-    app_dir = os.path.dirname(__file__)
-
-    if platform.architecture()[0] == "64bit":
-        sysdir = os.path.join(app_dir, 'dll', 'stdlib64')
-    else:
-        sysdir = os.path.join(app_dir, 'dll', 'stdlib')
-    # Python looks in sys.path for modules to load, insert new dir first in line.
-    sys.path.insert(0, sysdir)
-    os.environ['PATH'] = os.environ['PATH'] + ";."
-
-    ac.log(sysdir)
-    # print: apps/python/ac-drift-monitor\app\telemetry\memory\dll\stdlib64
-
-    from .lib.sim_info import info
-
-    return info
-
-
-info = _init1()
+_app_dir = os.path.dirname(os.path.abspath(__file__))
+_dll_dir_32 = os.path.join(_app_dir, 'dll', 'stdlib')
+_dll_dir_64 = os.path.join(_app_dir, 'dll', 'stdlib64')
+_dll_dir = _dll_dir_64 if platform.architecture()[0] == '64bit' else _dll_dir_32
+sys.path.insert(0, _dll_dir)
+from .lib.sim_info import info  # noqa: must append dll_dir before this import
 
 
 class Memory:
