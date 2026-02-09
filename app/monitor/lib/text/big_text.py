bf8795e3cacd9eae51fdd403b9c70434c985f739
Ibrewcoffee Onthemoon <ibrewcoffeeonthemoon@gmail.com>
2026-02-10 02:29:03 +0800

    BigText adds a speciial setter such that when text len change will auto adjust the font size and position

--- app/monitor/lib/text/big_text.py
+++ app/monitor/lib/text/big_text.py
@@ -4,23 +4,66 @@ from ....lib.number import num
 from ._base import Text
 
 
+class BigText(Text):
+    def __init__(
+        self,
+        x_pos: int,
+        y_pos: int,
+        width: int,
+        height: int,
+        text: str,
+        font_color: Color,
+        expected_text_len: int,
+    ) -> None:
+        super().__init__()
+        self._x_pos = x_pos
+        self._y_pos = y_pos
+        self._width = width
+        self._height = height
+        self._expected_text_len = None  # type: 'int | None'
+        # init label
+        self.text = text
+        self.font_color = font_color
+        self.font_alignment = 'center'
+        self.size = size(width, height)
+        self.expected_text_len = expected_text_len
+
+    @property
+    def _shrink_factor(self) -> float:
+        if not self.expected_text_len:
+            return 1.0
+        return num(1.0 - (self.expected_text_len - 1) * 0.25).clip(0.5, 1.0).f
+
+    @property
+    def expected_text_len(self) -> 'int | None':
+        return self._expected_text_len
+
+    @expected_text_len.setter
+    def expected_text_len(self, val: int) -> None:
+        if self._expected_text_len == val:
+            return
+        self._expected_text_len = val
+        # consequential adjustments
+        self.font_size = round(min(self._width, self._height)*self._shrink_factor)
+        vertical_offset = round(self._height/2-self._font_size*3/4)
+        self.position = position(self._x_pos, self._y_pos+vertical_offset)
+
+
 def big_text(
-    text: str,
     x_pos: int,
     y_pos: int,
     width: int,
     height: int,
+    text: str,
     font_color: Color,
     expected_text_len: int,
-) -> Text:
-    shrink_factor = num(1.0 - (expected_text_len - 1) * 0.25).clip(0.5, 1.0).f
-    font_size = min(width, height)*shrink_factor
-    font_size_vertical_offset = round(height/2-font_size*3/4)
-    return Text(
+) -> BigText:
+    return BigText(
+        x_pos,
+        y_pos,
+        width,
+        height,
         text=text,
-        font_size=round(font_size),
         font_color=font_color,
-        font_alignment='center',
-        size=size(width, height),
-        position=position(x_pos, y_pos+font_size_vertical_offset,),
+        expected_text_len=expected_text_len,
     )
