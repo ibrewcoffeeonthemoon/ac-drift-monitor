563672f480c4735680aabb3e9d3dcd184a902f91
Ibrewcoffee Onthemoon <ibrewcoffeeonthemoon@gmail.com>
2026-02-11 05:25:57 +0800

    move to angle_line() as a function

---
+++ app/monitor/lib/gl/angle.py
@@ -0,0 +1,26 @@
+import math
+
+from ....lib.color import *
+from ....lib.geometry import Vertex
+from .line import line
+
+
+def angle_line(
+    x_pos: int,
+    y_pos: int,
+    width: int,
+    height: int,
+    angle_degree: float,
+    color: Color,
+) -> None:
+    radian_angle = math.pi * angle_degree/180
+    x_start = x_pos+width//2
+    y_start = y_pos+height//2
+    radius = max(width, height)//2
+    x_coord = radius * math.cos(radian_angle)
+    y_coord = radius * math.sin(radian_angle)
+    line(
+        Vertex(x_start+x_coord, y_start-y_coord),
+        Vertex(x_start-x_coord, y_start+y_coord),
+        color=color,
+    )
--- app/monitor/slip_angle.py
+++ app/monitor/slip_angle.py
@@ -5,12 +5,10 @@ from acsys import CS
 import config
 
 from ..lib.color import *
-from ..lib.geometry import Vertex
-from ..lib.number import num
 from ..telemetry import ac_api
 from ._base import Monitor
 from .lib.chart import Chart
-from .lib.gl.line import line
+from .lib.gl.angle import angle_line
 
 
 class SlipAngleMonitor(Monitor):
@@ -55,14 +53,11 @@ class SlipAngleMonitor(Monitor):
         avg_rear_slipAngle = sum(ac_api[CS.SlipAngle].wma()[-2:])/2
 
         # plot the indicators
-        radian_angle = math.pi * avg_rear_slipAngle/180
-        radius = self.width//2
-        x_start = self._chart.x_pos+radius
-        y_start = self._chart.y_pos+radius
-        x_coord = radius * math.cos(radian_angle)
-        y_coord = radius * math.sin(radian_angle)
-        line(
-            Vertex(x_start+x_coord, y_start-y_coord),
-            Vertex(x_start-x_coord, y_start+y_coord),
-            color=red.full,
+        angle_line(
+            self._chart.x_pos,
+            self._chart.y_pos,
+            self._chart.width,
+            self._chart.height,
+            avg_rear_slipAngle,
+            yellow.full,
         )
