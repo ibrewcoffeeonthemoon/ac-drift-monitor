23bc457c1a2a18ac3ab67301d80380f207081f4f
Ibrewcoffee Onthemoon <ibrewcoffeeonthemoon@gmail.com>
2026-02-11 20:19:27 +0800

    simplify, chart.diagonal_len

--- app/monitor/lib/chart.py
+++ app/monitor/lib/chart.py
@@ -1,3 +1,5 @@
+import math
+
 import ac
 
 from ...lib.color import *
@@ -33,6 +35,7 @@ class Chart:
         self.corner_bottom_left = Vertex(x_pos, y_pos+height)
         self.corner_bottom_right = Vertex(x_pos+width, y_pos+height)
         self.center = Vertex(x_pos+width/2, y_pos+height/2)
+        self.diagonal_len = math.sqrt(width**2 + height**2)/2
         self._x_axis_color = x_axis_color
         self._y_axis_color = y_axis_color
         self._x_axis_marker_color = x_axis_marker_color
--- app/monitor/lib/indicator/angle_quad.py
+++ app/monitor/lib/indicator/angle_quad.py
@@ -30,7 +30,7 @@ class AngleQuad(Indicator):
     def _vertices(self, angle_degree: float) -> 'tuple[Vertex, Vertex, Vertex, Vertex]':
         radian_angle = math.pi * angle_degree/180
         x_center, y_center = self._chart.center
-        radius = math.sqrt(self._width**2 + self._height**2)/2
+        radius = self._chart.diagonal_len
         x_coord = radius * math.cos(radian_angle)
         y_coord = radius * math.sin(radian_angle)
         vertex1 = Vertex(
