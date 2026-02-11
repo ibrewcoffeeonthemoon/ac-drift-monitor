5cad65e5ff96e4e9af68416a237842acd6e9c906
Ibrewcoffee Onthemoon <ibrewcoffeeonthemoon@gmail.com>
2026-02-12 02:15:53 +0800

    AngleLine and AngleQuad preprocess the val before ploting

--- app/monitor/lib/indicator/angle/line.py
+++ app/monitor/lib/indicator/angle/line.py
@@ -1,19 +1,12 @@
 from .....lib.color import *
-from ...chart import Chart
 from ...gl.line import line
 from ._base import AngleIndicator
 
 
 class AngleLine(AngleIndicator):
-    def __init__(
-        self,
-        chart: Chart,
-        color: Color = yellow.full,
-    ) -> None:
-        super().__init__(chart=chart)
-        self._color = color
-
     def plot(self, val: float, color: 'Color | None' = None) -> None:
+        val = val * self._scale
+        val = -val if self._reversed else val
         line(
             *self._edge_intercepts(val),
             color=color or self._color
--- app/monitor/lib/indicator/angle/quad.py
+++ app/monitor/lib/indicator/angle/quad.py
@@ -1,25 +1,18 @@
 from .....lib.color import *
 from .....lib.geometry import Vertex
-from ...chart import Chart
 from ...gl.shape import quadrilateral
 from ._base import AngleIndicator
 
 
 class AngleQuad(AngleIndicator):
-    def __init__(
-        self,
-        chart: Chart,
-        color: Color = yellow.full,
-    ) -> None:
-        super().__init__(chart=chart)
-        self._color = color
-
     def _vertices(self, angle_degree: float) -> 'tuple[Vertex, Vertex, Vertex, Vertex]':
         vertex0, vertex1 = self._edge_intercepts(angle_degree)
         vertex2, vertex3 = self._nearest_corners(angle_degree)[-2:]
         return (vertex0, vertex1, vertex2, vertex3)
 
     def plot(self, val: float, color: 'Color | None' = None) -> None:
+        val = val * self._scale
+        val = -val if self._reversed else val
         quadrilateral(
             *self._vertices(val),
             color=color or self._color
