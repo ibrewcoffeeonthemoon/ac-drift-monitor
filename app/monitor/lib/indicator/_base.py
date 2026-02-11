f4965a1cdcfce44c1aa9e6a7ef577991b9c483ba
Ibrewcoffee Onthemoon <ibrewcoffeeonthemoon@gmail.com>
2026-02-11 20:30:55 +0800

    simplify, AngleIndicator as subclass, providing _edge_intercepts helper

--- app/monitor/lib/indicator/_base.py
+++ app/monitor/lib/indicator/_base.py
@@ -1,5 +1,8 @@
+import math
 from abc import ABCMeta, abstractmethod
 
+from ....lib.geometry import Vertex
+from ....lib.number import num
 from ..chart import Chart
 
 
@@ -41,3 +44,21 @@ class Indicator(metaclass=ABCMeta):
     @abstractmethod
     def plot(self, *args, **kwargs) -> None:
         ...
+
+
+class AngleIndicator(Indicator):
+    def _edge_intercepts(self, angle_degree: float) -> 'tuple[Vertex, Vertex]':
+        x_center, y_center = self._chart.center
+        radius = self._chart.diagonal_len
+        radian_angle = math.pi * angle_degree/180
+        x_coord = radius * math.cos(radian_angle)
+        y_coord = radius * math.sin(radian_angle)
+        vertex1 = Vertex(
+            num(x_center+x_coord).clip(self._x_pos, self._x_pos+self._width).f,
+            num(y_center-y_coord).clip(self._y_pos, self._y_pos+self._height).f,
+        )
+        vertex2 = Vertex(
+            num(x_center-x_coord).clip(self._x_pos, self._x_pos+self._width).f,
+            num(y_center+y_coord).clip(self._y_pos, self._y_pos+self._width).f,
+        )
+        return (vertex1, vertex2)
--- app/monitor/lib/indicator/angle_line.py
+++ app/monitor/lib/indicator/angle_line.py
@@ -1,14 +1,10 @@
-import math
-
 from ....lib.color import *
-from ....lib.geometry import Vertex
-from ....lib.number import num
 from ..chart import Chart
 from ..gl.line import line
-from ._base import Indicator
+from ._base import AngleIndicator
 
 
-class AngleLine(Indicator):
+class AngleLine(AngleIndicator):
     def __init__(
         self,
         chart: Chart,
@@ -27,25 +23,8 @@ class AngleLine(Indicator):
         )
         self._color = color
 
-    def _vertices(self, angle_degree: float) -> 'tuple[Vertex, Vertex]':
-        radian_angle = math.pi * angle_degree/180
-        x_center, y_center = self._chart.center
-        radius = self._chart.diagonal_len
-        x_coord = radius * math.cos(radian_angle)
-        y_coord = radius * math.sin(radian_angle)
-        return (
-            Vertex(
-                num(x_center+x_coord).clip(self._x_pos, self._x_pos+self._width).f,
-                num(y_center-y_coord).clip(self._y_pos, self._y_pos+self._height).f,
-            ),
-            Vertex(
-                num(x_center-x_coord).clip(self._x_pos, self._x_pos+self._width).f,
-                num(y_center+y_coord).clip(self._y_pos, self._y_pos+self._width).f,
-            ),
-        )
-
     def plot(self, val: float, color: 'Color | None' = None) -> None:
         line(
-            *self._vertices(val),
+            *self._edge_intercepts(val),
             color=color or self._color
         )
