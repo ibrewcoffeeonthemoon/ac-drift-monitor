1607cc6cf65e6eb52aa99dba17ada2ff394f57f2
Ibrewcoffee Onthemoon <ibrewcoffeeonthemoon@gmail.com>
2026-02-13 15:07:08 +0800

    draw some boundaries

--- app/monitor/tyre_info.py
+++ app/monitor/tyre_info.py
@@ -3,7 +3,7 @@ import config
 from ..lib.color import *
 from ..telemetry import ac_mem
 from ._base import Component, Monitor
-from .lib.canvas import Region
+from .lib.canvas import Chart, Region
 from .lib.text import BigText
 
 
@@ -11,23 +11,25 @@ class _TyreMonitor(Component):
     def __init__(
         self,
         i: int,
+        name: str,
         region: Region,
     ) -> None:
         self._i = i
 
         self._region = region
+        # BigText(self._region, name, white.a1, 5, 'center')
         self._temperature_text = BigText(
             region=self._region.top,
             text='',
             font_color=white.full,
-            expected_text_len=4,
+            expected_text_len=3,
         )
         BigText(self._region.top.right.right, '°c', white.a7, 3, 'left')
         self._pressure_text = BigText(
             region=self._region.bottom,
             text='',
             font_color=white.full,
-            expected_text_len=4,
+            expected_text_len=3,
         )
         BigText(self._region.bottom.right.right, 'psi', white.a7, 3, 'left')
 
@@ -37,8 +39,12 @@ class _TyreMonitor(Component):
         pressure = ac_mem.physics.wheelsPressure[self._i]  # type: float
 
         # plot the indicators
-        self._temperature_text.text = str(round(temperature, 1))
-        self._pressure_text.text = str(round(pressure, 1))
+        temperature_text = str(round(temperature, 1))
+        self._temperature_text.expected_text_len = len(temperature_text)-1
+        self._temperature_text.text = temperature_text
+        pressure_text = str(round(pressure, 1))
+        self._pressure_text.expected_text_len = len(pressure_text)-1
+        self._pressure_text.text = pressure_text
 
 
 class TyreInfoMonitor(Monitor):
@@ -56,14 +62,18 @@ class TyreInfoMonitor(Monitor):
         self._width = width = config.App.span_len*config.TyreInfoMonitor.col_span
         self._height = height = config.App.height
         self._region = Region(x_pos, y_pos, width, height)
-
+        self._chart = Chart(
+            self._region,
+            x_axis_color=white.a7,
+            y_axis_color=white.a7,
+        )
         self._tyre_monitors = [
-            _TyreMonitor(i, region)
-            for i, region in enumerate((
-                self._region.top_left,
-                self._region.top_right,
-                self._region.bottom_left,
-                self._region.bottom_right,
+            _TyreMonitor(i, name, region)
+            for i, (name, region) in enumerate((
+                ('FL', self._region.top_left),
+                ('FR', self._region.top_right),
+                ('RL', self._region.bottom_left),
+                ('RR', self._region.bottom_right),
             ))
         ]
 
@@ -73,5 +83,6 @@ class TyreInfoMonitor(Monitor):
     def height(self) -> float: return self._height
 
     def render(self) -> None:
+        self._chart.draw_axes()
         for monitor in self._tyre_monitors:
             monitor.render()
